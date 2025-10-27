34c6995d22df41c5e3c63de143bac90123423646
Scott Hyndman <scotty.hyndman@gmail.com>
2025-10-27 15:27:42 -0400

    Begin integrating logging

--- src/gemini_supply/log.py
+++ src/gemini_supply/log.py
@@ -1,14 +1,21 @@
-import logging
-import logfire
 from libsh import setup_logging_from_env
+from structlog import get_logger
 
 
 def setup_logging() -> None:
-  setup_logging_from_env()
-
   """Configure logging for the application."""
+  setup_logging_from_env()
+  get_logger().info("Logging initialized from environment")
+  _setup_logging_dependencies()
+
+
+def _setup_logging_dependencies() -> None:
+  import logging
+  import logfire
+
   # Suppress all logging from third-party libraries
   logging.getLogger("playwright-captcha").setLevel(logging.CRITICAL)
+  # Configure logfire to get their tendrils into some of our deps
   logfire.configure(console=logfire.ConsoleOptions(verbose=True))
   logfire.instrument_httpx()
   logfire.instrument_pydantic_ai()
